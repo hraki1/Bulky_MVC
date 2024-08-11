9ee7b3fc8ec272b5e97c49b6b6a4d790ad7ef524
hraki1 <ahmadhraki752@gmail.com>
2024-08-11 18:30:00 +0300

    Section 28

---
+++ BulkyWeb/Migrations/20240811142428_updtate-database.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updtatedatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "categories",
+                newName: "uyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "uyId",
+                table: "categories",
+                newName: "Id");
+        }
+    }
+}
