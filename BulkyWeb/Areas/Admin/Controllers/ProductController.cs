9dd22f1bd1d7661f382e24aee0a35c50aa04f39d
ahmad alhraki <ahmadhraki752@gmail.com>
2024-08-18 02:50:21 +0300

    Secetion 85

---
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll();
+            return View(products);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+
+            var category = _unitOfWork.Product.Get(u => u.Id == id);
+            return View(category);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(product);
+                _unitOfWork.Save();
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            var product = _unitOfWork.Product.Get(u => u.Id == id);
+            return View(product);
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            var product = _unitOfWork.Product.Get(u => u.Id == id);
+            _unitOfWork.Product.Remove(product);    
+            _unitOfWork.Save();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
